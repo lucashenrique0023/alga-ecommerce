2b19bf2e198a25c5941326b1b8753cd46e3149c0
lhss0023 <lucas.henrique0023@gmail.com>
2022-06-30 18:01:27 -0300

    9. JPQL: findById with Query and TypedQuery
    Query (Before JPA 2.0)

--- src/test/java/lab/lhss/ecommerce/jpql/BasicJPQL.java
+++ src/test/java/lab/lhss/ecommerce/jpql/BasicJPQL.java
@@ -5,6 +5,7 @@ import lab.lhss.ecommerce.model.Order;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -25,6 +26,21 @@ public class BasicJPQL extends EntityManagerTest {
 //        This avoid errors on empty results or multiple results
 //        List<Order> orderList = typedQuery.getResultList();
 //        Assert.assertFalse(orderList.isEmpty());
+    }
+
+    @Test
+    public void differenceBetweenQueryAndTypedQuery() {
+
+        String jpql = "select o from Order o where o.id = 1";
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(jpql, Order.class);
+        Order order1 = typedQuery.getSingleResult();
+        Assert.assertNotNull(order1);
+
+        Query query = entityManager.createQuery(jpql);
+        Order order2 = (Order) query.getSingleResult();
+        Assert.assertNotNull(order2);
+
     }
 
 }
