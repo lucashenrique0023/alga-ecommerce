c8c99cb65ce6a03bfcc913a1521bde53ade7fb48
lhss0023 <lucas.henrique0023@gmail.com>
2022-07-14 17:04:58 -0300

    9. JPQL: Create Named Queries on XML Files!

--- src/test/java/lab/lhss/ecommerce/jpql/NamedQuery.java
+++ src/test/java/lab/lhss/ecommerce/jpql/NamedQuery.java
@@ -2,6 +2,7 @@ package lab.lhss.ecommerce.jpql;
 
 import lab.lhss.ecommerce.EntityManagerTest;
 import lab.lhss.ecommerce.model.Item;
+import lab.lhss.ecommerce.model.Order;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,6 +18,21 @@ public class NamedQuery extends EntityManagerTest {
         typedQuery.setParameter("category", 2);
         List<Item> list = typedQuery.getResultList();
 
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void namedQueryFromFileTest() {
+        TypedQuery<Item> typedQuery = entityManager.createNamedQuery("Item.list", Item.class);
+        List<Item> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void namedQueryOrmXmlGetOrdersByClient() {
+        TypedQuery<Order> typedQuery = entityManager.createNamedQuery("Order.getByClient", Order.class);
+        typedQuery.setParameter("client", 1);
+        List<Order> list = typedQuery.getResultList();
         Assert.assertFalse(list.isEmpty());
     }
 }
