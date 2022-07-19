d269abea1780f1d9854f50c5c14c74f4941d0768
lhss0023 <lucas.henrique0023@gmail.com>
2022-07-19 12:46:40 -0300

    10. Criteria API: Get all products!

--- src/test/java/lab/lhss/ecommerce/criteria/BasicCriteriaTest.java
+++ src/test/java/lab/lhss/ecommerce/criteria/BasicCriteriaTest.java
@@ -2,6 +2,7 @@ package lab.lhss.ecommerce.criteria;
 
 import lab.lhss.ecommerce.EntityManagerTest;
 import lab.lhss.ecommerce.model.Client;
+import lab.lhss.ecommerce.model.Item;
 import lab.lhss.ecommerce.model.Order;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,8 +11,22 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class BasicCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void getAllItems() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+        Root<Item> root = criteriaQuery.from(Item.class);
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Item> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Item> list = typedQuery.getResultList();
+        Assert.assertNotNull(list);
+    }
 
     @Test
     public void findById() {
