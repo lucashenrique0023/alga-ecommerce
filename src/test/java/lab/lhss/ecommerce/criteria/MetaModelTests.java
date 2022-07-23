03e5dcf34f51beaad191de11ec982bdd9fb10293
lhss0023 <lucas.henrique0023@gmail.com>
2022-07-23 01:08:20 -0300

    10. Criteria API: Using Criteria With Metamodel

---
+++ src/test/java/lab/lhss/ecommerce/criteria/MetaModelTests.java
@@ -0,0 +1,34 @@
+package lab.lhss.ecommerce.criteria;
+
+import lab.lhss.ecommerce.EntityManagerTest;
+import lab.lhss.ecommerce.model.Item;
+import lab.lhss.ecommerce.model.Item_;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class MetaModelTests extends EntityManagerTest {
+
+    @Test
+    public void useMetaModel() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+        Root<Item> root = criteriaQuery.from(Item.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.or(
+                criteriaBuilder.like(root.get(Item_.name), "%C%"),
+                criteriaBuilder.like(root.get(Item_.description), "%C%")
+        ));
+
+        TypedQuery<Item> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Item> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+}
