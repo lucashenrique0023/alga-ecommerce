f08b737a4ab7bf032b518578293563cf1b690693
lhss0023 <lucas.henrique0023@gmail.com>
2022-09-11 09:35:35 -0300

    11. Native Queries: Return Entity
    To Return an Entity there are some rules:
    
    1. All Atributes of Entity must be returned from the Query! If the query does not have all Entity Attributes, use null and alias in the query to simulate attribute return.
    
    2. All Atributes names from the query must match names from the Entity, use alias if necessary.
    
    3. What matter is the attributes return, not where it comes from, you can join different classes on the query, but if the attributes match with Entity, there is no problem.

--- src/test/java/lab/lhss/ecommerce/nativequeries/NativeQueryTests.java
+++ src/test/java/lab/lhss/ecommerce/nativequeries/NativeQueryTests.java
@@ -1,6 +1,7 @@
 package lab.lhss.ecommerce.nativequeries;
 
 import lab.lhss.ecommerce.EntityManagerTest;
+import lab.lhss.ecommerce.model.Item;
 import org.junit.Test;
 
 import javax.persistence.Query;
@@ -18,6 +19,19 @@ public class NativeQueryTests extends EntityManagerTest {
         List<Object[]> result = query.getResultList();
 
         result.forEach(arr -> System.out.printf("Item -> ID: %s, Name: %s%n", arr[0], arr[1]));
+
+    }
+
+    @Test
+    public void returnEntity() {
+
+        String sql = "select * from item";
+
+        Query query = entityManager.createNativeQuery(sql, Item.class);
+
+        List<Item> result = query.getResultList();
+
+        result.forEach(item -> System.out.printf("Item -> ID: %s, Name: %s %n", item.getId(), item.getName()));
 
     }
 
