06882796505d84c037faa917d6737357a65f2521
lhss0023 <lucas.henrique0023@gmail.com>
2022-09-24 22:49:10 -0300

    12. EntityGraph
    Using EntityGraph to select relations dynamically with AttributeNodes.
    
    With this functionality we could use an entity with LAZY Relations and fetch then dynamically using AttributeNodes of EntityGraph.
    
    We used 2 types of graphs:
    
    fetchgraph: will select only the attributes specified ignoring the others EAGERS on entity, even explicit.
    
    loadgraph: will select the attributes specified and the others EAGERS defined on entity.

---
+++ src/test/java/lab/lhss/ecommerce/entitygraph/EntityGraphTests.java
@@ -0,0 +1,39 @@
+package lab.lhss.ecommerce.entitygraph;
+
+import lab.lhss.ecommerce.EntityManagerTest;
+import lab.lhss.ecommerce.model.Order;
+import lab.lhss.ecommerce.model.Order_;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.EntityGraph;
+import javax.persistence.TypedQuery;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EntityGraphTests extends EntityManagerTest {
+
+    @Test
+    public void findEssentialAttributesFromOrder() {
+
+        EntityGraph<Order> entityGraph = entityManager.createEntityGraph(Order.class);
+        entityGraph.addAttributeNodes(
+                Order_.CREATE_DATE, Order_.STATUS, Order_.TOTAL, Order_.INVOICE
+        );
+
+//        Map<String, Object> properties = new HashMap<>();
+//        properties.put("javax.persistence.fetchgraph", entityGraph);
+//        properties.put("javax.persistence.loadgraph", entityGraph);
+//        Order order = entityManager.find(Order.class, 1, properties);
+//        Assert.assertNotNull(order);
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery("select o from Order o", Order.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+
+
+    }
+
+}
