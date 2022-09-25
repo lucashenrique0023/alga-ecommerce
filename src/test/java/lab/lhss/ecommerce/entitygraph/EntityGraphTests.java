a6bdbae4e43905fc26c5a4f8f3096fd8416b830f
lhss0023 <lucas.henrique0023@gmail.com>
2022-09-25 00:14:55 -0300

    12. EntityGraph with Subgraph
    Subgraph will allow us to define dynamically for the others Entities in relation.

--- src/test/java/lab/lhss/ecommerce/entitygraph/EntityGraphTests.java
+++ src/test/java/lab/lhss/ecommerce/entitygraph/EntityGraphTests.java
@@ -1,18 +1,39 @@
 package lab.lhss.ecommerce.entitygraph;
 
 import lab.lhss.ecommerce.EntityManagerTest;
+import lab.lhss.ecommerce.model.Client;
 import lab.lhss.ecommerce.model.Order;
 import lab.lhss.ecommerce.model.Order_;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityGraph;
+import javax.persistence.Subgraph;
 import javax.persistence.TypedQuery;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EntityGraphTests extends EntityManagerTest {
+
+    @Test
+    public void findEssentialAttributesFromOrderWithSubGraph() {
+
+        EntityGraph<Order> entityGraph = entityManager.createEntityGraph(Order.class);
+        entityGraph.addAttributeNodes(
+                Order_.CREATE_DATE, Order_.STATUS, Order_.TOTAL, Order_.INVOICE
+        );
+
+        Subgraph<Client> subgraphClient = entityGraph.addSubgraph("client", Client.class);
+        subgraphClient.addAttributeNodes("name", "cpf");
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery("select o from Order o", Order.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+
+
+    }
 
     @Test
     public void findEssentialAttributesFromOrder() {
