c29252a9db57c654696fa93e6b8c583e31b4c69d
lhss0023 <lucas.henrique0023@gmail.com>
2022-09-27 10:30:23 -0300

    13. Cache Remove Entity from Cache JCache

--- src/test/java/lab/lhss/ecommerce/cache/CacheTests.java
+++ src/test/java/lab/lhss/ecommerce/cache/CacheTests.java
@@ -5,6 +5,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.persistence.Cache;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -43,6 +44,27 @@ public class CacheTests {
 
         System.out.println("Querying from instance 2.");
         entityManager2.find(Order.class, 1);
+
+    }
+
+    @Test
+    public void removeEntityFromCache() {
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        Cache cache = entityManagerFactory.getCache();
+
+        System.out.println("Querying from instance 1.");
+        entityManager1.createQuery("select o from Order o", Order.class).getResultList();
+
+        System.out.println("Removing Order 1 from Cache");
+        // cache.evict(Order.class, 1);  // Remove Specific entity with ID from cache
+        // cache.evict(Order.class); // Remove All Entries of Specific Entity from Cache
+        cache.evictAll(); // Remove All Entries of all Entities from cache
+
+        System.out.println("Querying from instance 2.");
+        entityManager2.find(Order.class, 1);
+        entityManager2.find(Order.class, 2);
 
     }
 
