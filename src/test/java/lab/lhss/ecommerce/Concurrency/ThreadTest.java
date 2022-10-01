04baf423a5f463f824d7b576bd48b2e74d5d8d29
lhss0023 <lucas.henrique0023@gmail.com>
2022-10-01 17:44:20 -0300

    13. Concurrency: Thread Example

---
+++ src/test/java/lab/lhss/ecommerce/Concurrency/ThreadTest.java
@@ -0,0 +1,48 @@
+package lab.lhss.ecommerce.Concurrency;
+
+import org.junit.Test;
+
+public class ThreadTest {
+
+    private static void log(Object obj, Object... args) {
+        System.out.println(
+                String.format("[LOG " + System.currentTimeMillis() + "] " + obj, args)
+        );
+    }
+
+    private static void waitSeconds(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {}
+    }
+
+    @Test
+    public void understandThreads() {
+        Runnable runnable1 = () -> {
+            log("Runnable 01 is going to wait 3 seconds.");
+            waitSeconds(3);
+            log("Runnable 01 finished.");
+        };
+
+        Runnable runnable2 = () -> {
+            log("Runnable 02 is going to wait 1 second.");
+            waitSeconds(1);
+            log("Runnable 02 finished.");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        log("End of test method.");
+    }
+}
